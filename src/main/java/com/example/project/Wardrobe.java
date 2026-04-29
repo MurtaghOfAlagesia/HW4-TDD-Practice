21aeacfe592a38bb5374a8739ac66dd1344eb598
MurtaghOfAlagesia <dp615531@wne.edu>
2026-04-29 02:50:23 +0000

    code: wardrobeLargerThan250ThrowsException

--- src/main/java/com/example/project/Wardrobe.java
+++ src/main/java/com/example/project/Wardrobe.java
@@ -1,1 +1,12 @@
+package com.example.project;
 
+public class Wardrobe {
+
+    public Wardrobe(int length) {
+        if (length > 250) {
+            throw new IllegalArgumentException("Wardobe is too big");
+        }
+    
+    }
+
+}
