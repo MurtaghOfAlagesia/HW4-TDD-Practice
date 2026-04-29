7036a5762ee90f72c211ca564489cce596f5337e
MurtaghOfAlagesia <dp615531@wne.edu>
2026-04-29 03:03:19 +0000

    clean: wardrobeLargerThan250ThrowsException

--- src/main/java/com/example/project/Wardrobe.java
+++ src/main/java/com/example/project/Wardrobe.java
@@ -2,9 +2,10 @@ package com.example.project;
 
 public class Wardrobe {
 
-    public Wardrobe(int length) {
-        if (length > 250) {
-            throw new IllegalArgumentException("Wardobe is too big");
+    public Wardrobe(int totalLength) {
+        int wallSize = 250;
+        if (totalLength > wallSize) {
+            throw new IllegalArgumentException("Wardobe is longer than wall");
         }
     
     }
