3c542022baffce879bc958ae2f376fb79032bdd6
MurtaghOfAlagesia <dp615531@wne.edu>
2026-04-29 02:39:21 +0000

    test: wardrobeLargerThan250ThrowsException

---
+++ src/test/java/com/example/project/WardrobeTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class WardrobeTest {
+
+    @Test
+    void wardrobeLargerThan250ThrowsException() {
+        assertThrows(IllegalArgumentException.class, 
+            () -> {new Wardrobe(300); },
+            "Wardrobe too large for wall does not raise exception");
+        };
+    }
+
